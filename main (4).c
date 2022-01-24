e359211dc0d8b884dbb14c1e7006d23ab8a4b10c
Reflexcoder <98248896+Reflexcoder@users.noreply.github.com>
2022-01-23 22:37:02 -0800

    Add files via upload

---
+++ main (4).c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main()
+{
+    float x,y;
+    char ch;
+    printf("Enter initial amount\n");
+    scanf("%f",&x);
+    printf("Enter \nc for credit\nd for debit \nb for balance\n ");
+    scanf("\n%c",&ch);
+    switch(ch)
+    {
+        case'c':
+        printf("Enter credit amount\n");
+        scanf("%f",&y);
+        x+=y;
+        printf("New amount=%f",x);
+        break;
+         case'd':
+        printf("Enter debit amount\n");
+        scanf("%f",&y);
+        if(x>=y)
+        {
+        x-=y;
+        printf("Nwe amount=%f",x);
+    }
+    else
+    {
+        printf("Insufficient amount in your account");
+    
+    }
+    break;
+    case 'b':
+    printf("Amount in your account=%f",x);
+    break;
+    default:
+    ("Choose correct option for operation");
+}
+
+    return 0;
+}
